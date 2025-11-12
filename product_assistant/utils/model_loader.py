eba254ba43313a82ffa958d424ed8e185a9fcc4e
Biro Kishore Das <birokishoredas@gmail.com>
2025-11-12 23:03:14 +0530

    Code updated

--- product_assistant/utils/model_loader.py
+++ product_assistant/utils/model_loader.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 from utils.config_loader import load_config
 from langchain_groq import ChatGroq
 from langchain_aws import BedrockEmbeddings
-from logger import GLOBAL_LOGGER as log
+from product_assistant.logger import GLOBAL_LOGGER as log
 from exception.custom_exception import ProductAssistantException
 import asyncio
 
